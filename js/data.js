7abcc0ae093f29b8421d31f952e0309d9a443742
parth.sarathi <iparthsarathi@gmail.com>
2026-04-18 02:33:32 +0530

    Update image URL for soft drink item

--- js/data.js
+++ js/data.js
@@ -619,7 +619,7 @@ const menuData = [
     hasSizes: false,
     price: 20,
     description: "Chilled soft drink can.",
-    image: " https://unsplash.com/photos/a-glass-filled-with-ice-on-top-of-a-table-qs0Z3QHoqOE",
+    image: " https://plus.unsplash.com/premium_photo-1725075086083-89117890371d?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     imageAlt: "Cold Soft Drink",
     isVeg: true,
     tag: "COLD DRINK"
